245fd680c9e68332c6e0a5c9f43f5a45088fe595
Angelo Castillo <castillo.angelo2024@gmail.com>
2026-05-28 11:26:08 +0800

    Add logo carousel back below hero

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import SplashScreen from "@/app/components/SplashScreen";
 import Navbar from "@/app/components/Navbar";
 import Hero from "@/app/components/Hero";
+import LogoCarousel from "@/app/components/LogoCarousel";
 import TechStack from "@/app/components/TechStack";
 import Projects from "@/app/components/Projects";
 import Experience from "@/app/components/Experience";
@@ -13,6 +14,7 @@ export default function Home() {
       <Navbar />
       <main>
         <Hero />
+        <LogoCarousel />
         <TechStack />
         <Projects />
         <Experience />
